f201d6509f2bd60f0c202a8575ccb7ae02af224b
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2023-06-10 18:15:50 +0300

    linter changes

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auto_service.settings')
     try:
@@ -9,8 +10,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and " +
             "available on your PYTHONPATH environment variable? Did you " +
-            "forget to activate a virtual environment?",
-)
+            "forget to activate a virtual environment?")
     execute_from_command_line(sys.argv)
 
 
